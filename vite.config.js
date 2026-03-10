5d4d9c7e70d304a79c033deaf740bf9f6015e1d6
Joseph Davin Christian <davin.chr123@gmail.com>
2026-03-10 09:24:31 +0700

    Update vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'Davinchr/mapping-mbg-bandung/',
+  base: '/mapping-mbg-bandung/',
   plugins: [react()],
   resolve: {
     alias: {
